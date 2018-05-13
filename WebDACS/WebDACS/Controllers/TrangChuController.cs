aa6066583d5c88cb0516a61cb3aac1508648713a
Hungbt <34257922+Hungbt@users.noreply.github.com>
2018-05-13 14:52:25 +0700

    hung

--- WebDACS/WebDACS/Controllers/TrangChuController.cs
+++ WebDACS/WebDACS/Controllers/TrangChuController.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebDACS.Models;
 
 namespace WebDACS.Controllers
 {
     public class TrangChuController : Controller
     {
         // GET: TrangChu
+        dbStockDataContext data = new dbStockDataContext();
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult product()
         {
-            return PartialView();
+            var model = (from a in data.STOCKs select a).ToList();
+            return PartialView(model);
+        }
+        public ActionResult ChuDe()
+        {
+            var model = (from a in data.CHUDEs select a).ToList();
+            return PartialView(model);
         }
     }
 }
